d30dd7c98dcb6c0935d511aec1e322186421e9b5
Gabriel Freire <gabrielfvale@gmail.com>
2018-06-08 20:35:09 -0300

    Set up code for planned features

--- logic.py
+++ logic.py
@@ -1,10 +1,12 @@
-from styles import *
-
-
 def isNumValid(matrix, num, row, col):
+	'''
+	Checa se ha numeros repetidos na mesma linha,
+	coluna ou secao da matriz do jogo.
+	'''
 	num = abs(num)
 	inicio = 0
 	fim = col
+	section = 9*[0]
 	starts = [0, 3, 6]
 	mids = [1, 4, 7]
 	ends = [2, 5, 8]
@@ -16,7 +18,7 @@ def isNumValid(matrix, num, row, col):
 	elif fim >= 6 and fim < 10:
 		inicio = 6
 		fim = 9
-	section = 9*[0]
+	# Constroi a secao
 	if row in starts:
 		section = matrix[row][inicio:fim]
 		section += matrix[row+1][inicio:fim]
@@ -30,6 +32,7 @@ def isNumValid(matrix, num, row, col):
 		section += matrix[row-1][inicio:fim]
 		section += matrix[row][inicio:fim]
 	cont = 0
+	# Por fim, checa todas as 3 condicoes de validacao
 	while cont < 9:
 		if matrix[row][cont] == num or matrix[cont][col] == num or section[cont] == num:
 			cont = 10
@@ -41,25 +44,38 @@ def isNumValid(matrix, num, row, col):
 
 
 def addTips(filename, matrix):
-	'''Abre o arquivo de configuracoes do Sudoku e insere as pistas.'''
+	'''Insere as pistas na matriz do Sudoku.'''
+	tips = getTips(filename)
+	for num in tips:
+		row, col, num = map(int, num.split(':'))
+		if isNumValid(matrix, num, row, col):
+			matrix[row][col] = num
+		else:
+			matrix[row][col] = -num
+
+
+def getTips(filename):
+	'''
+	Abre o arquivo de configuracoes, retornando 
+	os valores em uma lista melhor formatada.
+	'''
+	tips = []
 	colunas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
-	colors = getColors()
-	with open(filename + '.txt', 'r') as settings_obj:
+	with open(filename, 'r') as settings_obj:
 		for line in settings_obj:
 			line = line.strip()
 			row = int(line[2:3])-1
 			col = colunas.index(line[:1].lower())
 			num = int(line[-1])
-			if isNumValid(matrix, num, row, col):
-				matrix[row][col] = num
-			else:
-				matrix[row][col] = -num
+			tips.append('{}:{}:{}'.format(row, col, num))
+	return tips
 
 
 def catchError(matrix):
-  error = False
-  for i in range(9):
-    for j in range(9):
-      if matrix[i][j] < 0:
-        error = True
-  return error
+	'''Checa se há algum erro na matriz do jogo.'''
+	error = False
+	for i in range(9):
+		for j in range(9):
+			if matrix[i][j] < 0:
+				error = True
+	return error
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+
+'''
+TRABALHO FINAL - PYTHON3 - 2018.1
+Gabriel Freire do Vale, 418788
+Julia Hellen Rocha Vieira, 417425
+'''
+
 from os import system
 system('clear')
-from styles import *
+
 from logic import *
 import board
 
 MATRIZ = [[0 for i in range(9)] for i in range(9)]
 
-addTips('settings', MATRIZ)
-#print(buildBoard(MATRIZ), end="\r")
-cores = getColors()
+addTips('settings.txt', MATRIZ)
+tips = getTips('settings.txt')
 print(board.build(MATRIZ))
+print(tips)
